2e98929e7bb1a92414852dc6cb368cea2bd2e972
lili1A <154353393+lili1A@users.noreply.github.com>
2024-06-29 14:25:25 +0400

    using vs code

---
+++ sdvfsvsfvb.py
@@ -0,0 +1,3 @@
+print("This is the file I commit & push via Visual Studio Code.")
+print("The rest of the homework files i commited via uploading files directly from my device by using web version of Git Hub.")
+print("Now i am trying to see the result if the commit changes if i am using Git Hub or VS Code")
