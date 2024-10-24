2453c0499b9f5bfd95aba3d33921f7e6be639480
lili1A <154353393+lili1A@users.noreply.github.com>
2024-10-24 21:23:41 +0800

    hw movies pandas

--- pandas_movies.py
+++ pandas_movies.py
@@ -8,3 +8,16 @@ df = pd.read_csv(file_path)
 
 # Просмотр первых строк данных
 print(df.head())
+
+# наличие пропущенных значений
+print(df.isnull().sum())  # количество пропущенных значений в каждом столбце
+
+# для заполнения колонки votes, мне кажется, что лучше использовать mean() - среднее значение по столбцу
+# Заполнение пропущенных значений средним в колонке 'Votes'
+df['Votes'] = df['Votes'].fillna(df['Votes'].mean())
+# сохранение в таблицу
+df.to_csv('//Users/liliiagubaeva/Library/CloudStorage/OneDrive-AsiaPacificUniversity/pamdas_movies_hw.csv', index=False)
+print(df)
+# наверное логичнее заполнить revenue с помощью mean, но я хочу заполнить Медианное значение (median)
+df['Revenue (Millions)'] = df['Revenue (Millions)'].fillna(df['Revenue (Millions)'].median())
+print(df)
